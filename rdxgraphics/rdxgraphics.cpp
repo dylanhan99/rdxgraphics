5eb1c75e2cedb8d4e69311316b6cb9a3a556d66c
dylanhan99 <dylanhan99@gmail.com>
2025-04-14 01:55:40 +0800

    cmake glfw

---
+++ rdxgraphics/rdxgraphics.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "rdxgraphics.h"
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	int succ = glfwInit();
+
+	std::cout << succ << std::endl;
+
+	glfwTerminate();
+	return 0;
+}
