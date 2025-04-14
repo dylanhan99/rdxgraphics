889076d4cdb000ad2e73f12a296c992b133ca932
dylanhan99 <dylanhan99@gmail.com>
2025-04-14 12:48:20 +0800

    better support for cpp files

---
+++ rdxgraphics/entrypoint.cpp
@@ -0,0 +1,20 @@
+﻿#include <pch.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	int succ = glfwInit();
+	GLFWwindow* pWindow = glfwCreateWindow(600, 400, "Hello", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(pWindow))
+	{
+		glfwSwapBuffers(pWindow);
+		glfwPollEvents();
+
+		if (glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(pWindow, true);
+	}
+
+	glfwTerminate();
+	return 0;
+}
