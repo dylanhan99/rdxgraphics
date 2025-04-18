a76373eb60a88973eb0dcb0b24bb3b96156eb9fc
dylanhan99 <dylanhan99@gmail.com>
2025-04-18 10:51:47 +0800

    cmakelists workingdir preproc defs

--- rdxgraphics/RXGlobals.h
+++ rdxgraphics/RXGlobals.h
@@ -9,3 +9,11 @@ RX_DECLARE_EVENT(RX_EVENT_FRAMEBUFFER_RESIZE);
 RX_DECLARE_EVENT(RX_EVENT_CURSOR_POS_CALLBACK);
 RX_DECLARE_EVENT(RX_EVENT_SCROLL_CALLBACK);
 #undef RX_DELCARE_EVENT
+
+const std::filesystem::path g_WorkingDir{
+#ifdef RX_IS_IDE
+	RX_WORKING_DIR
+#else
+	std::filesystem::current_path()
+#endif
+};
