0fadb4fd1f6acafb69bbf2ab62c8609be35f06fe
dylanhan99 <dylanhan99@gmail.com>
2025-05-11 23:14:32 +0800

    removed non-tested collision checks

--- rdxgraphics/ECS/Systems/CollisionSystem.cpp
+++ rdxgraphics/ECS/Systems/CollisionSystem.cpp
@@ -66,15 +66,15 @@ bool CollisionSystem::CheckCollision(PointBV const& lhs, PointBV const& rhs)
 	return lhs.GetPosition() == rhs.GetPosition();
 }
 
-bool CollisionSystem::CheckCollision(PointBV const& lhs, RayBV const& rhs)
-{
-	glm::vec3 op = lhs.GetPosition() - rhs.GetPosition();
-	glm::vec3 dir = rhs.GetDirection();
-	float t = glm::dot(op, dir); // Is along the ray direction
-	bool isColinear = glm::length2(glm::cross(op, dir)) <glm::epsilon<float>();
-
-	return (t >= 0.f) && isColinear;
-}
+//bool CollisionSystem::CheckCollision(PointBV const& lhs, RayBV const& rhs)
+//{
+//	glm::vec3 op = lhs.GetPosition() - rhs.GetPosition();
+//	glm::vec3 dir = rhs.GetDirection();
+//	float t = glm::dot(op, dir); // Is along the ray direction
+//	bool isColinear = glm::length2(glm::cross(op, dir)) <glm::epsilon<float>();
+//
+//	return (t >= 0.f) && isColinear;
+//}
 
 bool CollisionSystem::CheckCollision(PointBV const& lhs, TriangleBV const& rhs)
 {
@@ -101,47 +101,6 @@ bool CollisionSystem::CheckCollision(PointBV const& lhs, SphereBV const& rhs)
 {
 	float d2 = glm::distance2(lhs.GetPosition(), rhs.GetPosition());
 	return d2 < glm::pow(rhs.GetRadius(), 2.f);
-}
-
-bool CollisionSystem::CheckCollision(RayBV const& lhs, PointBV const& rhs) { return CheckCollision(rhs, lhs); }
-
-// NOT WORKING
-bool CollisionSystem::CheckCollision(RayBV const& lhs, RayBV const& rhs)
-{
-	// 1. Coplanar
-	// 2. Both directions do intersect
-
-	glm::vec3 p1 = lhs.GetPosition();
-	glm::vec3 p2 = rhs.GetPosition();
-	glm::vec3 d1 = glm::normalize(lhs.GetDirection());
-	glm::vec3 d2 = glm::normalize(rhs.GetDirection());
-
-	// Colinear check
-	glm::vec3 r = p1 - p2;
-	glm::vec3 crossDir = glm::cross(d1, d2);
-	float eps2 = glm::pow(glm::epsilon<float>(), 2.f);
-	if (glm::length2(crossDir) < eps2)
-	{ // ||crossDir|| == 0.f if the (normalized) the directional vectors are parallel
-		glm::vec3 crossR = glm::cross(r, d1);
-		return glm::length2(crossR) < eps2;
-	}
-
-	// Not parallel, must check minimum distance between both rays.
-	// Not parallel — check minimal distance
-	float a = glm::dot(d1, d1);
-	float b = glm::dot(d1, d2);
-	float c = glm::dot(d2, d2);
-	float d = glm::dot(d1, r);
-	float e = glm::dot(d2, r);
-
-	float denom = a * c - b * b;
-	float t1 = (b * e - c * d) / denom;
-	float t2 = (a * e - b * d) / denom;
-
-	glm::vec3 j = p1 + t1 * d1;
-	glm::vec3 k = p2 + t2 * d2;
-
-	return glm::length2(j - k) < eps2;
 }
 
 bool CollisionSystem::CheckCollision(RayBV const& lhs, TriangleBV const& rhs)
@@ -180,36 +139,9 @@ bool CollisionSystem::CheckCollision(TriangleBV const& lhs, PointBV const& rhs) 
 
 bool CollisionSystem::CheckCollision(TriangleBV const& lhs, RayBV const& rhs) { return CheckCollision(rhs, lhs); }
 
-bool CollisionSystem::CheckCollision(TriangleBV const& lhs, TriangleBV const& rhs)
-{
-	return false;
-}
-
-bool CollisionSystem::CheckCollision(TriangleBV const& lhs, PlaneBV const& rhs)
-{
-	return false;
-}
-
-bool CollisionSystem::CheckCollision(TriangleBV const& lhs, AABBBV const& rhs)
-{
-	return false;
-}
-
-bool CollisionSystem::CheckCollision(TriangleBV const& lhs, SphereBV const& rhs)
-{
-	return false;
-}
-
 bool CollisionSystem::CheckCollision(PlaneBV const& lhs, PointBV const& rhs) { return CheckCollision(rhs, lhs); }
 
 bool CollisionSystem::CheckCollision(PlaneBV const& lhs, RayBV const& rhs) { return CheckCollision(rhs, lhs); }
-
-bool CollisionSystem::CheckCollision(PlaneBV const& lhs, TriangleBV const& rhs) { return CheckCollision(rhs, lhs); }
-
-bool CollisionSystem::CheckCollision(PlaneBV const& lhs, PlaneBV const& rhs)
-{
-	return false;
-}
 
 bool CollisionSystem::CheckCollision(PlaneBV const& lhs, AABBBV const& rhs)
 {
@@ -224,8 +156,6 @@ bool CollisionSystem::CheckCollision(PlaneBV const& lhs, SphereBV const& rhs)
 bool CollisionSystem::CheckCollision(AABBBV const& lhs, PointBV const& rhs) { return CheckCollision(rhs, lhs); }
 
 bool CollisionSystem::CheckCollision(AABBBV const& lhs, RayBV const& rhs) { return CheckCollision(rhs, lhs); }
-
-bool CollisionSystem::CheckCollision(AABBBV const& lhs, TriangleBV const& rhs) { return CheckCollision(rhs, lhs); }
 
 bool CollisionSystem::CheckCollision(AABBBV const& lhs, PlaneBV const& rhs) { return CheckCollision(rhs, lhs); }
 
@@ -263,8 +193,6 @@ bool CollisionSystem::CheckCollision(AABBBV const& lhs, SphereBV const& rhs)
 bool CollisionSystem::CheckCollision(SphereBV const& lhs, PointBV const& rhs) { return CheckCollision(rhs, lhs); }
 
 bool CollisionSystem::CheckCollision(SphereBV const& lhs, RayBV const& rhs) { return CheckCollision(rhs, lhs); }
-
-bool CollisionSystem::CheckCollision(SphereBV const& lhs, TriangleBV const& rhs) { return CheckCollision(rhs, lhs); }
 
 bool CollisionSystem::CheckCollision(SphereBV const& lhs, PlaneBV const& rhs) { return CheckCollision(rhs, lhs); }
 
--- rdxgraphics/ECS/Systems/CollisionSystem.h
+++ rdxgraphics/ECS/Systems/CollisionSystem.h
@@ -8,15 +8,23 @@ public:
 	static void Update(float dt);
 
 private:
+	// Default will come here.
+	template <typename T, typename U>
+	static bool CheckCollision(T const& lhs, U const& rhs)
+	{
+		return false;
+	}
+	// All supported intersection tests follow.
+
 	static bool CheckCollision(PointBV const& lhs, PointBV const& rhs);
-	static bool CheckCollision(PointBV const& lhs, RayBV const& rhs);
+	//static bool CheckCollision(PointBV const& lhs, RayBV const& rhs);
 	static bool CheckCollision(PointBV const& lhs, TriangleBV const& rhs);
 	static bool CheckCollision(PointBV const& lhs, PlaneBV const& rhs);
 	static bool CheckCollision(PointBV const& lhs, AABBBV const& rhs);
 	static bool CheckCollision(PointBV const& lhs, SphereBV const& rhs);
 
-	static bool CheckCollision(RayBV const& lhs, PointBV const& rhs);
-	static bool CheckCollision(RayBV const& lhs, RayBV const& rhs);
+	//static bool CheckCollision(RayBV const& lhs, PointBV const& rhs);
+	//static bool CheckCollision(RayBV const& lhs, RayBV const& rhs);
 	static bool CheckCollision(RayBV const& lhs, TriangleBV const& rhs);
 	static bool CheckCollision(RayBV const& lhs, PlaneBV const& rhs);
 	static bool CheckCollision(RayBV const& lhs, AABBBV const& rhs);
@@ -24,28 +32,28 @@ private:
 
 	static bool CheckCollision(TriangleBV const& lhs, PointBV const& rhs);
 	static bool CheckCollision(TriangleBV const& lhs, RayBV const& rhs);
-	static bool CheckCollision(TriangleBV const& lhs, TriangleBV const& rhs);
-	static bool CheckCollision(TriangleBV const& lhs, PlaneBV const& rhs);
-	static bool CheckCollision(TriangleBV const& lhs, AABBBV const& rhs);
-	static bool CheckCollision(TriangleBV const& lhs, SphereBV const& rhs);
+	//static bool CheckCollision(TriangleBV const& lhs, TriangleBV const& rhs);
+	//static bool CheckCollision(TriangleBV const& lhs, PlaneBV const& rhs);
+	//static bool CheckCollision(TriangleBV const& lhs, AABBBV const& rhs);
+	//static bool CheckCollision(TriangleBV const& lhs, SphereBV const& rhs);
 
 	static bool CheckCollision(PlaneBV const& lhs, PointBV const& rhs);
 	static bool CheckCollision(PlaneBV const& lhs, RayBV const& rhs);
-	static bool CheckCollision(PlaneBV const& lhs, TriangleBV const& rhs);
-	static bool CheckCollision(PlaneBV const& lhs, PlaneBV const& rhs);
+	//static bool CheckCollision(PlaneBV const& lhs, TriangleBV const& rhs);
+	//static bool CheckCollision(PlaneBV const& lhs, PlaneBV const& rhs);
 	static bool CheckCollision(PlaneBV const& lhs, AABBBV const& rhs);
 	static bool CheckCollision(PlaneBV const& lhs, SphereBV const& rhs);
 
 	static bool CheckCollision(AABBBV const& lhs, PointBV const& rhs);
 	static bool CheckCollision(AABBBV const& lhs, RayBV const& rhs);
-	static bool CheckCollision(AABBBV const& lhs, TriangleBV const& rhs);
+	//static bool CheckCollision(AABBBV const& lhs, TriangleBV const& rhs);
 	static bool CheckCollision(AABBBV const& lhs, PlaneBV const& rhs);
 	static bool CheckCollision(AABBBV const& lhs, AABBBV const& rhs);
 	static bool CheckCollision(AABBBV const& lhs, SphereBV const& rhs);
 
 	static bool CheckCollision(SphereBV const& lhs, PointBV const& rhs);
 	static bool CheckCollision(SphereBV const& lhs, RayBV const& rhs);
-	static bool CheckCollision(SphereBV const& lhs, TriangleBV const& rhs);
+	//static bool CheckCollision(SphereBV const& lhs, TriangleBV const& rhs);
 	static bool CheckCollision(SphereBV const& lhs, PlaneBV const& rhs);
 	static bool CheckCollision(SphereBV const& lhs, AABBBV const& rhs);
 	static bool CheckCollision(SphereBV const& lhs, SphereBV const& rhs);
