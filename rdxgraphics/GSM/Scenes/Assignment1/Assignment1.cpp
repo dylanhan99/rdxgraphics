7a1357f3555cf9354bee8996d75ecae0477cb3af
dylanhan99 <dylanhan99@gmail.com>
2025-05-23 23:40:55 +0800

    tests done

--- rdxgraphics/GSM/Scenes/Assignment1/Assignment1.cpp
+++ rdxgraphics/GSM/Scenes/Assignment1/Assignment1.cpp
@@ -11,7 +11,7 @@ void SphereXSphereScene::StartImpl()
 {
 	{
 		auto handle = BaseScene::CreateDefaultEntity();
-		EntityManager::AddComponent<Metadata>(handle, "Move Me!");
+		EntityManager::AddComponent<Metadata>(handle, "(Sphere) Move Me!");
 		EntityManager::AddComponent<Xform>(handle, glm::vec3{0.f}, glm::vec3{0.5f});
 		EntityManager::AddComponent<Model>(handle, Shape::Sphere);
 		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
@@ -28,7 +28,7 @@ void SphereXSphereScene::StartImpl()
 	int begin = -halfWidth;
 	int end = halfWidth;
 
-	std::string name = "Ent ";
+	std::string name = "Sphere ";
 	for (int i = begin; i < end; ++i)
 	{
 		for (int j = begin; j < end; ++j)
@@ -61,7 +61,7 @@ void AABBXSphereScene::StartImpl()
 {
 	{
 		auto handle = BaseScene::CreateDefaultEntity();
-		EntityManager::AddComponent<Metadata>(handle, "Move Me!");
+		EntityManager::AddComponent<Metadata>(handle, "(AABB) Move Me!");
 		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
 		EntityManager::AddComponent<Model>(handle, Shape::Cube);
 		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
@@ -78,7 +78,7 @@ void AABBXSphereScene::StartImpl()
 	int begin = -halfWidth;
 	int end = halfWidth;
 
-	std::string name = "Ent ";
+	std::string name = "Sphere ";
 	for (int i = begin; i < end; ++i)
 	{
 		for (int j = begin; j < end; ++j)
@@ -111,7 +111,7 @@ void AABBXAABBScene::StartImpl()
 {
 	{
 		auto handle = BaseScene::CreateDefaultEntity();
-		EntityManager::AddComponent<Metadata>(handle, "Move Me!");
+		EntityManager::AddComponent<Metadata>(handle, "(AABB) Move Me!");
 		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
 		EntityManager::AddComponent<Model>(handle, Shape::Cube);
 		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
@@ -128,7 +128,7 @@ void AABBXAABBScene::StartImpl()
 	int begin = -halfWidth;
 	int end = halfWidth;
 
-	std::string name = "Ent ";
+	std::string name = "AABB ";
 	for (int i = begin; i < end; ++i)
 	{
 		for (int j = begin; j < end; ++j)
@@ -161,7 +161,7 @@ void PointXSphereScene::StartImpl()
 {
 	{
 		auto handle = BaseScene::CreateDefaultEntity();
-		EntityManager::AddComponent<Metadata>(handle, "Move Me!");
+		EntityManager::AddComponent<Metadata>(handle, "(Point) Move Me!");
 		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
 		EntityManager::AddComponent<Model>(handle, Shape::Cube);
 		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
@@ -177,7 +177,7 @@ void PointXSphereScene::StartImpl()
 	int begin = -halfWidth;
 	int end = halfWidth;
 
-	std::string name = "Ent ";
+	std::string name = "Sphere ";
 	for (int i = begin; i < end; ++i)
 	{
 		for (int j = begin; j < end; ++j)
@@ -210,7 +210,7 @@ void PointXAABBScene::StartImpl()
 {
 	{
 		auto handle = BaseScene::CreateDefaultEntity();
-		EntityManager::AddComponent<Metadata>(handle, "Move Me!");
+		EntityManager::AddComponent<Metadata>(handle, "(Point) Move Me!");
 		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
 		EntityManager::AddComponent<Model>(handle, Shape::Cube);
 		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
@@ -226,7 +226,7 @@ void PointXAABBScene::StartImpl()
 	int begin = -halfWidth;
 	int end = halfWidth;
 
-	std::string name = "Ent ";
+	std::string name = "AABB ";
 	for (int i = begin; i < end; ++i)
 	{
 		for (int j = begin; j < end; ++j)
@@ -257,32 +257,399 @@ void PointXAABBScene::StartImpl()
 
 void PointXTriangleScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Triangle) Move my points!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Triangle);
+		auto& prim = EntityManager::GetComponent<TrianglePrimitive>(handle);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "Point ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Cube);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::Point);
+			}
+		}
+	}
 }
 
 void PointXPlaneScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Plane) Spin Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "Point ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Cube);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::Point);
+			}
+		}
+	}
 }
 
 void RayXSphereScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Ray) Point Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Ray);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "Sphere ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Sphere);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::Sphere);
+			}
+		}
+	}
 }
 
 void RayXAABBScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Ray) Point Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Ray);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "AABB ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Cube);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::AABB);
+			}
+		}
+	}
 }
 
 void RayXTriangleScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Ray) Point Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Ray);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "Triangle ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Triangle);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::Triangle);
+			}
+		}
+	}
 }
 
 void RayXPlaneScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Ray) Point Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Ray);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "Plane (Top)");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f, 10.f, -5.f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+		auto& prim = EntityManager::GetComponent<PlanePrimitive>(handle);
+		prim.GetOrientation() = { -glm::half_pi<float>(), 0.f, 0.f};
+	}
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "Plane (Bottom)");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f, -10.f, -5.f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+		auto& prim = EntityManager::GetComponent<PlanePrimitive>(handle);
+		prim.GetOrientation() = { glm::half_pi<float>(), 0.f, 0.f};
+	}
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "Plane (Left)");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ -10.f, 0.f, -5.f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+		auto& prim = EntityManager::GetComponent<PlanePrimitive>(handle);
+		prim.GetOrientation() = { 0.f, glm::half_pi<float>(), 0.f};
+	}
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "Plane (Right)");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 10.f, 0.f, -5.f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+		auto& prim = EntityManager::GetComponent<PlanePrimitive>(handle);
+		prim.GetOrientation() = { 0.f, -glm::half_pi<float>(), 0.f};
+	}
 }
 
 void PlaneXAABBScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Plane) Spin Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "AABB ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Cube);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::AABB);
+			}
+		}
+	}
 }
 
 void PlaneXSphereScene::StartImpl()
 {
+	{
+		auto handle = BaseScene::CreateDefaultEntity();
+		EntityManager::AddComponent<Metadata>(handle, "(Plane) Spin Me!");
+		EntityManager::AddComponent<Xform>(handle, glm::vec3{ 0.f }, glm::vec3{ 0.5f });
+		EntityManager::AddComponent<Model>(handle, Shape::Cube);
+		EntityManager::AddComponent<Material>(handle, glm::vec3{ 0.f, 0.f, 1.f });
+		EntityManager::AddComponent<Collider>(handle, Primitive::Plane);
+
+		GUI::SetSelectedEntity(handle);
+	}
+
+
+	int halfWidth = 1.f;
+	int step = 4.f;
+
+	int begin = -halfWidth;
+	int end = halfWidth;
+
+	std::string name = "Sphere ";
+	for (int i = begin; i < end; ++i)
+	{
+		for (int j = begin; j < end; ++j)
+		{
+			for (int k = begin; k < end; ++k)
+			{
+				glm::vec3 pos{
+					(float)i * step,
+					(float)j * step,
+					(float)k * step
+				};
+
+				std::string n = name +
+					"[" + std::to_string(i + halfWidth) + "]" +
+					"[" + std::to_string(j + halfWidth) + "]" +
+					"[" + std::to_string(k + halfWidth) + "]";
+
+				auto handle = BaseScene::CreateDefaultEntity();
+				EntityManager::AddComponent<Metadata>(handle, n);
+				EntityManager::AddComponent<Xform>(handle, pos, glm::vec3{ 0.25f });
+				EntityManager::AddComponent<Model>(handle, Shape::Sphere);
+				EntityManager::AddComponent<Material>(handle, glm::vec3{ 1.f });
+				EntityManager::AddComponent<Collider>(handle, Primitive::Sphere);
+			}
+		}
+	}
 }
