ad4c734d28f3df4432db6327b3561a58319efc2b
dylanhan99 <dylanhan99@gmail.com>
2025-12-02 13:54:09 +0800

    Quick and dirty FPS throttler and displayer (proof of concept)

--- projects/rdxengine/RDX.cpp
+++ projects/rdxengine/RDX.cpp
@@ -33,93 +33,140 @@ int RDX::Run()
 				ServiceLayer::WindowSystem()->SetShouldClose();
 			});
 
+		static uint64_t frame = 0;
+		auto lastTime = std::chrono::system_clock::now();
+		float dt{};
+		constexpr int targetFrames = 30;
+		constexpr float targetDt = 1.f / targetFrames;
+		int currentFPS{};
 		while (!ServiceLayer::WindowSystem()->IsWindowShouldClose())
 		{
-			RX_PROFILE_FRAME(1);
+			++frame;
+			{
+				auto now = std::chrono::system_clock::now();
+				auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - lastTime);
+				lastTime = now;
+				dt = elapsed.count() * 0.001f; // Convert ms to seconds
+			}
+			{ // FPS value displayer. Resets every 1 second
+				static float dtAccum = 0.f;
+				static int numFrames = 0;
+
+				dtAccum += dt;
+				++numFrames;
+
+				if (dtAccum >= 1000.f)
+				{
+					currentFPS = dtAccum / numFrames;
+					dtAccum = 0.f;
+					numFrames = 0;
+					std::cout << currentFPS << "\n";
+				}
+			}
+
+			RX_PROFILE_FRAME(frame);
 			RX_PROFILE("Main Loop");
 			ServiceLayer::WindowSystem()->PollEvents();
 
 			ServiceLayer::ApplicationService()->FrameStart();
 
-			if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::Escape))
 			{
-				ServiceLayer::InstantEventService()->Publish(ShutdownEngineEvent{"ESCAPE!"});
-			}
-
-			if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::A))
-			{
-				for (int i = 0; i < 1000; ++i)
+				if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::Escape))
 				{
-					RX_TRACE("Test {}", i);
+					ServiceLayer::InstantEventService()->Publish(ShutdownEngineEvent{ "ESCAPE!" });
 				}
-			}
 
-			if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::S))
-			{
-				ServiceLayer::EntityComponentService()->Clear();
-			}
-
-			if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::D))
-			{
-				//constexpr int sz = 3;
-				//for (int i = -sz/2; i < sz/2; ++i)
-				//{
-				//	Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
-				//	auto pXform = ent.AddComponent<TransformComponent>();
-				//	if (pXform)
-				//	{
-				//		auto& xform = *pXform;
-				//		xform.Position.x = i;
-				//		xform.Position.y = i;
-				//		xform.Position.z = 0.f;
-				//		//RX_DEBUG("{}, {}, {}", xform.Position.x, xform.Position.y, xform.Position.z);
-				//	}
-				//}
+				if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::A))
 				{
-					Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
-					auto pXform = ent.AddComponent<TransformComponent>();
-					if (pXform)
+					for (int i = 0; i < 1000; ++i)
 					{
-						auto& xform = *pXform;
-						xform.Position.x = -1.f;
-						xform.Position.y = -1.f;
-						xform.Position.z = -1.f;
+						RX_TRACE("Test {}", i);
 					}
 				}
+
+				if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::S))
 				{
-					Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
-					auto pXform = ent.AddComponent<TransformComponent>();
-					if (pXform)
-					{
-						auto& xform = *pXform;
-						xform.Position.x = 0.f;
-						xform.Position.y = 0.f;
-						xform.Position.z = 0.f;
-					}
+					ServiceLayer::EntityComponentService()->Clear();
 				}
+
+				if (ServiceLayer::InputService()->IsKeyTriggered(KeyCode::D))
 				{
-					Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
-					auto pXform = ent.AddComponent<TransformComponent>();
-					if (pXform)
+					//constexpr int sz = 3;
+					//for (int i = -sz/2; i < sz/2; ++i)
+					//{
+					//	Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
+					//	auto pXform = ent.AddComponent<TransformComponent>();
+					//	if (pXform)
+					//	{
+					//		auto& xform = *pXform;
+					//		xform.Position.x = i;
+					//		xform.Position.y = i;
+					//		xform.Position.z = 0.f;
+					//		//RX_DEBUG("{}, {}, {}", xform.Position.x, xform.Position.y, xform.Position.z);
+					//	}
+					//}
 					{
-						auto& xform = *pXform;
-						xform.Position.x = 1.f;
-						xform.Position.y = 1.f;
-						xform.Position.z = 1.f;
+						Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
+						auto pXform = ent.AddComponent<TransformComponent>();
+						if (pXform)
+						{
+							auto& xform = *pXform;
+							xform.Position.x = -1.f;
+							xform.Position.y = -1.f;
+							xform.Position.z = -1.f;
+						}
+					}
+					{
+						Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
+						auto pXform = ent.AddComponent<TransformComponent>();
+						if (pXform)
+						{
+							auto& xform = *pXform;
+							xform.Position.x = 0.f;
+							xform.Position.y = 0.f;
+							xform.Position.z = 0.f;
+						}
+					}
+					{
+						Entity ent = ServiceLayer::EntityComponentService()->CreateEntity();
+						auto pXform = ent.AddComponent<TransformComponent>();
+						if (pXform)
+						{
+							auto& xform = *pXform;
+							xform.Position.x = 1.f;
+							xform.Position.y = 1.f;
+							xform.Position.z = 1.f;
+						}
 					}
 				}
 			}
-
 
 			for (BaseSystem* p : ServiceLayer::GetSystems())
-				p->Update(0.0015f);
+				p->Update(dt);
 
 			for (BaseSystem* p : ServiceLayer::GetSystems())
 				p->Draw();
 
-			ServiceLayer::PerformanceProfilingService()->Update(0.00015f);
+			ServiceLayer::PerformanceProfilingService()->Update(dt);
 			ServiceLayer::ApplicationService()->FrameEnd();
 			ServiceLayer::WindowSystem()->SwapBuffers();
+
+#if 0
+			{
+				static float accum{};
+				accum += dt;
+				std::cout << accum << "\n";
+			}
+#endif
+			
+			{
+				// Becareful of a pure sleep_until approach. The OS may sleep for +-[2,5]ms, which is inconsequential at lower fps
+				// But at higher target fps, the 2ms really adds up, maybe a 244fps may only perform at 150+fps
+				// Not really important right now, but something to thinka bout next time.
+				using namespace std::chrono;
+				auto end = lastTime + round<milliseconds>(duration<float>(targetDt)); // The "correct" next frame time
+				std::this_thread::sleep_until(end);
+			}
 		}
 	}
 	else
